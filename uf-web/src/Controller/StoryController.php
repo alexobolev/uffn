961137a2385cb661e0cf2ca7756907f3403b4b91
Alex Sobolev <code@sobolev.fi>
2022-03-10 08:41:02 +0200

    Add fulltext and table of contents pages

--- uf-web/src/Controller/StoryController.php
+++ uf-web/src/Controller/StoryController.php
@@ -25,6 +25,15 @@ class StoryController extends AbstractController {
         return $this->render('web/story/versions.html.twig', []);
     }
 
+
+    /**
+     * Display a single chapter with story/version info.
+     * This is the default starting story display.
+     *
+     * @param int $storyId      Story instance index.
+     * @param int $versionId    Version instance index, must belong to the story.
+     * @param int $chapterNum   Chapter index, must belong to the version.
+     */
     #[Route(
         '/stories/{storyId}/versions/{versionId}/chapters/{chapterNum}',
         name: 'story_version_chapter',
@@ -58,7 +67,7 @@ class StoryController extends AbstractController {
             );
         }
 
-        return $this->render('web/story/chapter.html.twig', [
+        return $this->render('web/story/chapters.html.twig', [
             'story' => [
                 'info' => [
                     'title' => $version->getTitle(),
@@ -95,9 +104,131 @@ class StoryController extends AbstractController {
             'chapters' => array_slice($chapters->getValues(), $chapterNum - 1, 1),
             'bottom_nav' => [
                 'prev_index' => ($chapterNum > 1) ? $chapterNum - 1 : null,
-                'next_index' => ($chapterNum < count($chapters)) ? $chapterNum + 1 : null,
-                'chapters' => $chapters
+                'next_index' => ($chapterNum < count($chapters)) ? $chapterNum + 1 : null
             ]
+        ]);
+    }
+
+
+    /**
+     * Display all chapters, with story/version info on top.
+     *
+     * @param int $storyId      Story instance index.
+     * @param int $versionId    Version instance index, must belong to the story.
+     */
+    #[Route(
+        '/stories/{storyId}/versions/{versionId}/fulltext',
+        name: 'story_version_fulltext',
+        requirements: ['storyId' => '\d+', 'versionId' => '\d+']
+    )]
+    public function storyVersionFullText(int $storyId, int $versionId) {
+        $story = $this->doctrine->getRepository(Story::class)->find(id: $storyId);
+        if (!$story) {
+            throw $this->createNotFoundException (
+                'No story found for id = ' . $storyId
+            );
+        }
+
+        $version = $this->doctrine->getRepository(Version::class)->find(id: $versionId);
+        if (!$version || $version->getStory() != $story) {
+            throw $this->createNotFoundException (
+                'No version found for id = ' . $versionId . ' or it belongs to a different story'
+            );
+        }
+
+        $chapters = $version->getChapters();
+        if (count($chapters) == 0) {
+            throw $this->createNotFoundException (
+                'No chapters found for version with id = ' . $versionId
+            );
+        }
+
+        return $this->render('web/story/fulltext.html.twig', [
+            'story' => [
+                'info' => [
+                    'title' => $version->getTitle(),
+                    'authors' => array_map(
+                        fn($author): string => $author->getName(),
+                        $version->getAuthors()->getValues()),
+                    'story_id' => $storyId,
+                    'version_id' => $versionId,
+                    'identifier' => $story->getOriginIdentifier()
+                ],
+                'meta' => [
+                    'rating' => $story->getRating() ?? $version->getRating(),
+                    'chapter_count' => count($chapters),
+                    'word_count' => 100500,
+                    'is_complete' => $version->getIsCompleted(),
+                    'updated_on' => $version->getUpdatedAt()->format('d/m/Y'),
+                ],
+                'custom_summary' => $story->getSummary(),
+                'original_summary' => $version->getSummary()
+            ],
+            'user_actions' => [
+                'info' => [
+                    'retrieved_on' => $version->getArchivedAt()->format('d/m/Y H:i:s'),
+                    'link' => sprintf('https://archiveofourown.org/works/%d', $story->getOriginIdentifier()),
+                    'mobile_link_name' => sprintf('%s://%d',
+                        $story->getOriginArchive()->value,
+                        $story->getOriginIdentifier())
+                ]
+            ],
+            'chapters' => $chapters
+        ]);
+    }
+
+
+    #[Route(
+        '/stories/{storyId}/versions/{versionId}/toc',
+        name: 'story_version_toc',
+        requirements: ['storyId' => '\d+', 'versionId' => '\d+']
+    )]
+    public function storyVersionTOC(int $storyId, int $versionId) {
+        $story = $this->doctrine->getRepository(Story::class)->find(id: $storyId);
+        if (!$story) {
+            throw $this->createNotFoundException (
+                'No story found for id = ' . $storyId
+            );
+        }
+
+        $version = $this->doctrine->getRepository(Version::class)->find(id: $versionId);
+        if (!$version || $version->getStory() != $story) {
+            throw $this->createNotFoundException (
+                'No version found for id = ' . $versionId . ' or it belongs to a different story'
+            );
+        }
+
+        $chapters = $version->getChapters()->getValues();
+        if (count($chapters) == 0) {
+            throw $this->createNotFoundException (
+                'No chapters found for version with id = ' . $versionId
+            );
+        }
+
+        usort($chapters, fn($lhs, $rhs) => $lhs->getSequenceNum() > $rhs->getSequenceNum());
+
+        return $this->render('web/story/toc.html.twig', [
+            'story' => [
+                'info' => [
+                    'title' => $version->getTitle(),
+                    'authors' => array_map(
+                        fn($author): string => $author->getName(),
+                        $version->getAuthors()->getValues()),
+                    'story_id' => $storyId,
+                    'version_id' => $versionId,
+                    'identifier' => $story->getOriginIdentifier()
+                ]
+            ],
+            'user_actions' => [
+                'info' => [
+                    'retrieved_on' => $version->getArchivedAt()->format('d/m/Y H:i:s'),
+                    'link' => sprintf('https://archiveofourown.org/works/%d', $story->getOriginIdentifier()),
+                    'mobile_link_name' => sprintf('%s://%d',
+                        $story->getOriginArchive()->value,
+                        $story->getOriginIdentifier())
+                ]
+            ],
+            'chapters' => $chapters
         ]);
     }
 }
